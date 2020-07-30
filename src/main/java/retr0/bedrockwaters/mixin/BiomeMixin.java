700aa098e810669d1bcd5024b16a799defc08499
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2020-07-30 15:41:16 -0700

    Fixed improper mixin formatting

--- src/main/java/retr0/bedrockwaters/mixin/BiomeMixin.java
+++ src/main/java/retr0/bedrockwaters/mixin/BiomeMixin.java
@@ -2,12 +2,14 @@ package retr0.bedrockwaters.mixin;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeEffects;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import retr0.bedrockwaters.BiomeExt;
 
 @Mixin(Biome.class)
 public abstract class BiomeMixin implements BiomeExt {
-    public BiomeEffects effects;
+    @Shadow @Final protected BiomeEffects effects;
 
     public void setWaterAttributes(int waterColor, int waterFogColor) {
         this.effects.waterColor = waterColor;
