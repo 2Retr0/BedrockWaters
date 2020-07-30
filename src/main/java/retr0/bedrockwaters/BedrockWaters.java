20d48aed12924001f00b6e8fffd690cf573762f7
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2020-07-30 13:53:09 -0700

    Altered biome properties replacement logic

--- src/main/java/retr0/bedrockwaters/BedrockWaters.java
+++ src/main/java/retr0/bedrockwaters/BedrockWaters.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
 public class BedrockWaters implements ModInitializer {
@@ -32,6 +33,7 @@ public class BedrockWaters implements ModInitializer {
 
         // loop through all registered biomes
         Set<Identifier> biomes = Registry.BIOME.getIds();
+        Set<Identifier> childBiomes = new HashSet<>();
         for(Identifier biomeId : biomes) {
             Config.BiomeConfig biomeConfig = Config.getBiomeFromBiomeConfig(biomeId);
             Config.BiomeConfig defaultConfig = Config.getDefaultBiomeConfig();
@@ -44,24 +46,31 @@ public class BedrockWaters implements ModInitializer {
                 ((BiomeExt) Registry.BIOME.get(biomeId)).setWaterAttributes(waterColor, waterFogColor);
                 log(Level.DEBUG,"Changed " + biomeId + "properties -> WaterColor: #" + Integer.toHexString(waterColor) + ", WaterFogColor: #" + Integer.toHexString(waterFogColor));
             }
-            // otherwise if biome has a parent, use parent biome properties
+            /* for child biomes to inherit the color properties of the parent biome, we need to make sure that
+             * parent biomes have their properties already replaced */
             else if ((biomeId.getNamespace().equals("minecraft") || !Config.allowVanillaBiomesOnly())
                     && Config.canChildBiomeInheritParentColors()
                     && Registry.BIOME.get(biomeId).hasParent()) {
-                waterColor = Registry.BIOME.get(Identifier.tryParse(Registry.BIOME.get(biomeId).getParent())).getWaterColor();
-                waterFogColor = Registry.BIOME.get(Identifier.tryParse(Registry.BIOME.get(biomeId).getParent())).getWaterFogColor();
-
-                ((BiomeExt) Registry.BIOME.get(biomeId)).setWaterAttributes(waterColor, waterFogColor);
-                log(Level.DEBUG,"Changed " + biomeId + "properties -> WaterColor: #" + Integer.toHexString(waterColor) + ", WaterFogColor: #" + Integer.toHexString(waterFogColor));
+                childBiomes.add(biomeId);
             }
             // otherwise use default biome properties
-            else if (biomeId.getNamespace().equals("minecraft") || !Config.allowVanillaBiomesOnly()) {
+            else if ((biomeId.getNamespace().equals("minecraft") || !Config.allowVanillaBiomesOnly())
+                    && defaultConfig != null) {
                 waterColor = hexToInt(defaultConfig.getBiomeProperties().getWaterColor());
                 waterFogColor = hexToInt(defaultConfig.getBiomeProperties().getWaterFogColor());
 
                 ((BiomeExt) Registry.BIOME.get(biomeId)).setWaterAttributes(waterColor, waterFogColor);
                 log(Level.DEBUG, "Changed " + biomeId + "properties -> WaterColor: #" + Integer.toHexString(waterColor) + ", WaterFogColor: #" + Integer.toHexString(waterFogColor));
             }
+        }
+
+        // replace child biome properties from parent biome
+        for(Identifier childBiomeId : childBiomes) {
+            waterColor = Registry.BIOME.get(Identifier.tryParse(Registry.BIOME.get(childBiomeId).getParent())).getWaterColor();
+            waterFogColor = Registry.BIOME.get(Identifier.tryParse(Registry.BIOME.get(childBiomeId).getParent())).getWaterFogColor();
+
+            ((BiomeExt) Registry.BIOME.get(childBiomeId)).setWaterAttributes(waterColor, waterFogColor);
+            log(Level.DEBUG,"Changed " + childBiomeId + "properties -> WaterColor: #" + Integer.toHexString(waterColor) + ", WaterFogColor: #" + Integer.toHexString(waterFogColor));
         }
     }
 
