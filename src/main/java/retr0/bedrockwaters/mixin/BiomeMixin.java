8e9c60af849230793948efa9bb1708d700d8e8e1
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2020-08-18 02:50:39 -0700

    Replace references of BiomeExt with IBiome
    - Reworked setWaterAttributes for support with the @Accessor interface as well

--- src/main/java/retr0/bedrockwaters/mixin/BiomeMixin.java
+++ src/main/java/retr0/bedrockwaters/mixin/BiomeMixin.java
@@ -5,14 +5,14 @@ import net.minecraft.world.biome.BiomeEffects;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import retr0.bedrockwaters.BiomeExt;
+import retr0.bedrockwaters.IBiome;
 
 @Mixin(Biome.class)
-public abstract class BiomeMixin implements BiomeExt {
+public abstract class BiomeMixin implements IBiome {
     @Shadow @Final private BiomeEffects effects;
 
     public void setWaterAttributes(int waterColor, int waterFogColor) {
-        this.effects.waterColor = waterColor;
-        this.effects.waterFogColor = waterFogColor;
+        ((BiomeEffectsMixin) this.effects).setWaterColor(waterColor);
+        ((BiomeEffectsMixin) this.effects).setWaterFogColor(waterFogColor);
     }
 }
