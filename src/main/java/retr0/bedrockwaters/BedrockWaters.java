cc5b45bdb3be71f37cbb31aab9da6b2125dcc574
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2021-01-06 02:21:31 -0800

    Fixed issue with dedicated servers failing to edit biome water colors.
    * The mod will now need to be run server-side for biome water colors to be edited.
    * This will have the additional effect of allowing players without the mod to see edited water colors while on the server, however, underwater fog transitions and Bedrock Edition water textures are left client-side.

--- src/main/java/retr0/bedrockwaters/BedrockWaters.java
+++ src/main/java/retr0/bedrockwaters/BedrockWaters.java
@@ -1,11 +1,15 @@
 package retr0.bedrockwaters;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
+import net.fabricmc.fabric.mixin.biome.VanillaLayeredBiomeSourceAccessor;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.world.biome.Biome;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,15 +29,17 @@ public class BedrockWaters implements ModInitializer {
     public void onInitialize() {
         log(Level.INFO, "BedrockWaters initialized!");
 
-        // TODO: Lower water sprite opacity
-        final Identifier bedrockWaterStillSpriteId = new Identifier(MOD_ID, "block/water_still");
-        final Identifier bedrockWaterFlowSpriteId = new Identifier(MOD_ID, "block/water_flow");
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            // TODO: Lower water sprite opacity
+            final Identifier bedrockWaterStillSpriteId = new Identifier(MOD_ID, "block/water_still");
+            final Identifier bedrockWaterFlowSpriteId = new Identifier(MOD_ID, "block/water_flow");
 
-        // add Bedrock Edition water sprites to the block atlas
-        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX).register((atlasTexture, registry) -> {
-            registry.register(bedrockWaterStillSpriteId);
-            registry.register(bedrockWaterFlowSpriteId);
-        });
+            // add Bedrock Edition water sprites to the block atlas
+            ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).register((atlasTexture, registry) -> {
+                registry.register(bedrockWaterStillSpriteId);
+                registry.register(bedrockWaterFlowSpriteId);
+            });
+        }
 
         try {
             Config.init(new File("./config/"+MOD_ID+"/"));
@@ -42,25 +48,29 @@ public class BedrockWaters implements ModInitializer {
         }
 
         log(Level.INFO, "Modifying vanilla biome water attributes...");
-        // loop through all registered vanilla biomes
-        BuiltinRegistries.BIOME.forEach(Biome -> {
-            WaterPropertiesReplacer.setBiomeWaterProperties(Biome);
-            log(Level.DEBUG, "Modified " + BuiltinRegistries.BIOME.getId(Biome) + " properties -> "
-                    + "WaterColor: #" + Integer.toHexString(Biome.getWaterColor()) + ", "
-                    + "WaterFogColor: #" + Integer.toHexString(Biome.getWaterFogColor()));
+        VanillaLayeredBiomeSourceAccessor.getBIOMES().forEach(biomeKey -> {
+            Biome currentBiome = BuiltinRegistries.BIOME.get(biomeKey.getValue());
+            String properties = (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) ?
+                    " properties -> WaterColor: #" + Integer.toHexString(currentBiome.getWaterColor()) +
+                            ", WaterFogColor: #" + Integer.toHexString(currentBiome.getWaterFogColor()) : "";
+
+            WaterPropertiesReplacer.setBiomeWaterProperties(currentBiome);
+            log(Level.DEBUG, "Modified " + biomeKey.toString() + properties);
         });
         log(Level.INFO, "Finished!");
 
         // for modded biomes, we register a callback event which changes their water properties accordingly
         RegistryEntryAddedCallback.event(BuiltinRegistries.BIOME).register((i, identifier, biome) -> {
+            String properties = (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) ?
+                    " properties -> WaterColor: #" + Integer.toHexString(biome.getWaterColor()) +
+                            ", WaterFogColor: #" + Integer.toHexString(biome.getWaterFogColor()) : "";
+
             setBiomeWaterProperties(biome);
-            log(Level.DEBUG, "Modified " + identifier + " properties -> "
-                    + "WaterColor: #" + Integer.toHexString(biome.getWaterColor()) + ", "
-                    + "WaterFogColor: #" + Integer.toHexString(biome.getWaterFogColor()));
+            log(Level.DEBUG, "Modified " + identifier + properties);
         });
     }
 
-    public static void log(Level level, String message){
+    public static void log(Level level, String message) {
         LOGGER.log(level, "["+MOD_NAME+"] " + message);
     }
 }
