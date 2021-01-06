5ab4a7926e11bc865547f460472f1aab5396dd05
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2021-01-06 02:35:59 -0800

    Fixed having not all vanilla biomes edited.

--- src/main/java/retr0/bedrockwaters/BedrockWaters.java
+++ src/main/java/retr0/bedrockwaters/BedrockWaters.java
@@ -1,21 +1,24 @@
 package retr0.bedrockwaters;
 
+import com.google.common.collect.ImmutableList;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
-import net.fabricmc.fabric.mixin.biome.VanillaLayeredBiomeSourceAccessor;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import static retr0.bedrockwaters.WaterPropertiesReplacer.setBiomeWaterProperties;
 
@@ -24,6 +27,8 @@ public class BedrockWaters implements ModInitializer {
 
     public static final String MOD_ID = "bedrockwaters";
     public static final String MOD_NAME = "BedrockWaters";
+
+    private static final List<RegistryKey<Biome>> BIOMES;
 
     @Override
     public void onInitialize() {
@@ -48,7 +53,7 @@ public class BedrockWaters implements ModInitializer {
         }
 
         log(Level.INFO, "Modifying vanilla biome water attributes...");
-        VanillaLayeredBiomeSourceAccessor.getBIOMES().forEach(biomeKey -> {
+        BIOMES.forEach(biomeKey -> {
             Biome currentBiome = BuiltinRegistries.BIOME.get(biomeKey.getValue());
             String properties = (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) ?
                     " properties -> WaterColor: #" + Integer.toHexString(currentBiome.getWaterColor()) +
@@ -72,5 +77,90 @@ public class BedrockWaters implements ModInitializer {
 
     public static void log(Level level, String message) {
         LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+    // I have absolutely no idea where to find the full list of default biomes, so we're just gonna hard code it for now...
+    static {
+        BIOMES = ImmutableList.of(
+            BiomeKeys.OCEAN,
+            BiomeKeys.PLAINS,
+            BiomeKeys.DESERT,
+            BiomeKeys.MOUNTAINS,
+            BiomeKeys.FOREST,
+            BiomeKeys.TAIGA,
+            BiomeKeys.SWAMP,
+            BiomeKeys.RIVER,
+            BiomeKeys.NETHER_WASTES,
+            BiomeKeys.THE_END,
+            BiomeKeys.FROZEN_OCEAN,
+            BiomeKeys.FROZEN_RIVER,
+            BiomeKeys.SNOWY_TUNDRA,
+            BiomeKeys.SNOWY_MOUNTAINS,
+            BiomeKeys.MUSHROOM_FIELDS,
+            BiomeKeys.MUSHROOM_FIELD_SHORE,
+            BiomeKeys.BEACH,
+            BiomeKeys.DESERT_HILLS,
+            BiomeKeys.WOODED_HILLS,
+            BiomeKeys.TAIGA_HILLS,
+            BiomeKeys.MOUNTAIN_EDGE,
+            BiomeKeys.JUNGLE,
+            BiomeKeys.JUNGLE_HILLS,
+            BiomeKeys.JUNGLE_EDGE,
+            BiomeKeys.DEEP_OCEAN,
+            BiomeKeys.STONE_SHORE,
+            BiomeKeys.SNOWY_BEACH,
+            BiomeKeys.BIRCH_FOREST,
+            BiomeKeys.BIRCH_FOREST_HILLS,
+            BiomeKeys.DARK_FOREST,
+            BiomeKeys.SNOWY_TAIGA,
+            BiomeKeys.SNOWY_TAIGA_HILLS,
+            BiomeKeys.GIANT_TREE_TAIGA,
+            BiomeKeys.GIANT_TREE_TAIGA_HILLS,
+            BiomeKeys.WOODED_MOUNTAINS,
+            BiomeKeys.SAVANNA,
+            BiomeKeys.SAVANNA_PLATEAU,
+            BiomeKeys.BADLANDS,
+            BiomeKeys.WOODED_BADLANDS_PLATEAU,
+            BiomeKeys.BADLANDS_PLATEAU,
+            BiomeKeys.SMALL_END_ISLANDS,
+            BiomeKeys.END_MIDLANDS,
+            BiomeKeys.END_HIGHLANDS,
+            BiomeKeys.END_BARRENS,
+            BiomeKeys.WARM_OCEAN,
+            BiomeKeys.LUKEWARM_OCEAN,
+            BiomeKeys.COLD_OCEAN,
+            BiomeKeys.DEEP_WARM_OCEAN,
+            BiomeKeys.DEEP_LUKEWARM_OCEAN,
+            BiomeKeys.DEEP_COLD_OCEAN,
+            BiomeKeys.DEEP_FROZEN_OCEAN,
+            BiomeKeys.THE_VOID,
+            BiomeKeys.SUNFLOWER_PLAINS,
+            BiomeKeys.DESERT_LAKES,
+            BiomeKeys.GRAVELLY_MOUNTAINS,
+            BiomeKeys.FLOWER_FOREST,
+            BiomeKeys.TAIGA_MOUNTAINS,
+            BiomeKeys.SWAMP_HILLS,
+            BiomeKeys.ICE_SPIKES,
+            BiomeKeys.MODIFIED_JUNGLE,
+            BiomeKeys.MODIFIED_JUNGLE_EDGE,
+            BiomeKeys.TALL_BIRCH_FOREST,
+            BiomeKeys.TALL_BIRCH_HILLS,
+            BiomeKeys.DARK_FOREST_HILLS,
+            BiomeKeys.SNOWY_TAIGA_MOUNTAINS,
+            BiomeKeys.GIANT_SPRUCE_TAIGA,
+            BiomeKeys.GIANT_SPRUCE_TAIGA_HILLS,
+            BiomeKeys.MODIFIED_GRAVELLY_MOUNTAINS,
+            BiomeKeys.SHATTERED_SAVANNA,
+            BiomeKeys.SHATTERED_SAVANNA_PLATEAU,
+            BiomeKeys.ERODED_BADLANDS,
+            BiomeKeys.MODIFIED_WOODED_BADLANDS_PLATEAU,
+            BiomeKeys.MODIFIED_BADLANDS_PLATEAU,
+            BiomeKeys.BAMBOO_JUNGLE,
+            BiomeKeys.BAMBOO_JUNGLE_HILLS,
+            BiomeKeys.SOUL_SAND_VALLEY,
+            BiomeKeys.CRIMSON_FOREST,
+            BiomeKeys.WARPED_FOREST,
+            BiomeKeys.BASALT_DELTAS
+        );
     }
 }
