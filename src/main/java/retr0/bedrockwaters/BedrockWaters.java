e8dc146f61a7bde5df1f65c0d6c067d968784123
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2022-09-13 14:03:24 -0700

    Update Logger and introduce asset detection.

--- src/main/java/retr0/bedrockwaters/BedrockWaters.java
+++ src/main/java/retr0/bedrockwaters/BedrockWaters.java
@@ -2,37 +2,75 @@ package retr0.bedrockwaters;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourcePack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.RegistryEntry;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import retr0.bedrockwaters.mixin.BiomeColorsAccessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.Stream;
+
+import static net.fabricmc.fabric.api.resource.ResourcePackActivationType.DEFAULT_ENABLED;
+import static net.minecraft.resource.ResourceType.CLIENT_RESOURCES;
 
 public class BedrockWaters implements ClientModInitializer {
-    public static Logger LOGGER         = LogManager.getLogger();
-
-    public static final String MOD_ID   = "bedrockwaters";
+    public static final String MOD_ID = "bedrockwaters";
     public static final String MOD_NAME = "BedrockWaters";
+
+    public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static boolean areAssetsLoaded;
 
     @Override
     public void onInitializeClient() {
-        log(Level.INFO, "BedrockWaters initialized!");
-        // TODO: Lower water sprite opacity
+        LOGGER.info(MOD_NAME + " initialized!");
 
-        FabricLoader.getInstance().getModContainer(MOD_ID)
-                .map(container -> ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID, "resources"),
-                        container, ResourcePackActivationType.DEFAULT_ENABLED))
-                .filter(success -> !success).ifPresent(success -> LOGGER.warn("Could not register built-in resource pack."));
-
-        BiomeColorsAccessor.setWaterColor((biome, x, z) -> WaterPropertiesReplacer.getBiomeWaterProperties(RegistryEntry.of(biome), false));
-    }
+        // Loading the BedrockWater's default resource pack.
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(
+                    new Identifier(MOD_ID, "resources"), modContainer, DEFAULT_ENABLED);
+        });
 
 
 
-    public static void log(Level level, String message) {
-        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+        // Register a resource reload listener for determining whether the mod's assets have been overwritten or not.
+        ResourceManagerHelper.get(CLIENT_RESOURCES).registerReloadListener(
+            new SimpleSynchronousResourceReloadListener() {
+                /**
+                 * Returns <code>true</code> if a given resource pack contains assets present in the BedrockWater's
+                 * resource pack.
+                 */
+                private boolean packHasAssets(ResourcePack pack) {
+                    var assetIds = Stream.of("water_flow.png", "water_still.png")
+                        .map(n -> new Identifier("textures/block/" + n));
+
+                    return assetIds.anyMatch(id -> pack.contains(CLIENT_RESOURCES, id));
+                }
+
+                @Override
+                public Identifier getFabricId() { return new Identifier(MOD_ID, "resources"); }
+
+
+                @Override
+                public void reload(ResourceManager manager) {
+                    areAssetsLoaded = false;
+
+                    // We consider that the mod's assets are loaded if these conditions are met:
+                    //   * BedrockWaters resource pack is loaded.
+                    //   * The BedrockWaters resource pack does not have its assets overwritten by another pack.
+                    try (var packStream = manager.streamResourcePacks()) {
+                        packStream.forEachOrdered(pack -> {
+                            if (pack.getName().contains(MOD_ID))
+                                areAssetsLoaded = true;
+                            else if (areAssetsLoaded && packHasAssets(pack))
+                                areAssetsLoaded = false;
+                        });
+                    } catch (Exception e) {
+                        LOGGER.error("Error occurred while processing resource packs: ", e);
+                    }
+                }
+            });
     }
 }
