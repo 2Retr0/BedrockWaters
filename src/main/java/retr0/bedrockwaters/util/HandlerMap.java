2874d1ecfc8553daa770c8080818ec561bf24099
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2022-09-13 14:07:46 -0700

    Introduce helper utilities.

---
+++ src/main/java/retr0/bedrockwaters/util/HandlerMap.java
@@ -0,0 +1,38 @@
+package retr0.bedrockwaters.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * A utility which promotes cleaner control flow at the cost of some performance (when compared to an if-else control
+ * flow counterpart). Specifically designed for handling possible arguments for an instance method.
+ * @param handlerMappings A mapping from keys to single-argument handlers.
+ * @param keys A {@link Stream<K>} of keys originally contained in handlerMappings.
+ * @param defaultValue A fallback value for when no mapping can be found.
+ * @param <K> The key type used in handlerMappings.
+ * @param <A> The argument type for the single-argument handlers.
+ * @param <B> The return type for handlers.
+ */
+public record HandlerMap <K, A, B> (LinkedHashMap<K, Function<A, B>> handlerMappings, Stream<K> keys, B defaultValue)
+{
+    public HandlerMap(Map<K, Function<A, B>> handlerMappings, B defaultValue) {
+        this(new LinkedHashMap<>(handlerMappings), handlerMappings.keySet().stream(), defaultValue);
+    }
+
+
+
+    /**
+     * Handles a value based on the assigned mappings with its handler key determined by the specified selector.
+     * @param value The value to be handled. This is the value to be passed into the proper handler if it exists.
+     * @param selector A {@link Predicate<K>} which will determine the handler key to select.
+     * @return The return value from the handler or the assigned default value if no handler could be found.
+     */
+    public B handle(A value, Predicate<K> selector) {
+        var matchedKey = keys.filter(selector).findFirst();
+
+        return (matchedKey.isEmpty()) ? defaultValue : handlerMappings.get(matchedKey.get()).apply(value);
+    }
+}
---
+++ src/main/java/retr0/bedrockwaters/util/SmoothStepUtil.java
@@ -0,0 +1,58 @@
+package retr0.bedrockwaters.util;
+
+import net.minecraft.util.Util;
+
+import static net.minecraft.util.math.MathHelper.*;
+
+/**
+ * A utility which performs Hermite interpolation between two values over the course of a specified duration.
+ */
+public record SmoothStepUtil(float initialValue, float targetValue, float deltaTime, long startTime) {
+    /**
+     * Initializes a new SmoothStepUtil instance.
+     * @param initialValue The initial value for interpolation.
+     * @param targetValue The target value for interpolation.
+     * @param deltaTime The total time in milliseconds over which the interpolation should occur.
+     * @param offset An offset time in milliseconds before interpolation would start (defaults to {@code 0f}).
+     */
+    public SmoothStepUtil(float initialValue, float targetValue, float deltaTime, float offset) {
+        this(initialValue, targetValue, deltaTime, Util.getMeasuringTimeMs() + ((long) offset));
+    }
+
+    /**
+     * @see SmoothStepUtil#SmoothStepUtil
+     */
+    public SmoothStepUtil(float initialValue, float targetValue, float deltaTime) {
+        this(initialValue, targetValue, deltaTime, 0f);
+    }
+
+    /**
+     * Used to represent an already completed transition.
+     * @see SmoothStepUtil#SmoothStepUtil
+     */
+    public SmoothStepUtil(float finalValue) { this(finalValue, finalValue, 0.001f, 0L); }
+
+
+
+    /**
+     * @return The current value based on interpolation progress.
+     */
+    public float currentValue() {
+        // Smooth step between start and target values if the target value is not yet reached.
+        var currentDelta = clamp((Util.getMeasuringTimeMs() - startTime) / deltaTime, 0f, 1f);
+
+        return (currentDelta > 1f) ? targetValue : smoothStep(currentDelta, initialValue, targetValue);
+    }
+
+
+
+    /**
+     * Performs Hermite interpolation between two values as used in GLSL's smoothstep().
+     * <a href="https://en.wikipedia.org/wiki/Smoothstep">Source</a>
+     * @param t The interpolation parameter between 0 and 1.
+     * @param a The initial value for interpolation.
+     * @param b The target value for interpolation.
+     * @return The current value based on interpolation progress.
+     */
+    public static float smoothStep(float t, float a, float b) { return lerp(t * t * (3f - 2f * t), a, b); }
+}
