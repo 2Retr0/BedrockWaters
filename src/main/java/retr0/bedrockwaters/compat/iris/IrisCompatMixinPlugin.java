871224c14e72753ad578541d7a7863bd49ee5530
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2022-09-18 11:34:51 -0700

    Move redundant plugin methods to an abstract class.

--- src/main/java/retr0/bedrockwaters/compat/iris/IrisCompatMixinPlugin.java
+++ src/main/java/retr0/bedrockwaters/compat/iris/IrisCompatMixinPlugin.java
@@ -2,14 +2,9 @@ package retr0.bedrockwaters.compat.iris;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import retr0.bedrockwaters.compat.AbstractMixinPlugin;
 
-import java.util.List;
-import java.util.Set;
-
-public class IrisCompatMixinPlugin implements IMixinConfigPlugin {
+public class IrisCompatMixinPlugin extends AbstractMixinPlugin {
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
         var loader = FabricLoader.getInstance();
@@ -17,24 +12,4 @@ public class IrisCompatMixinPlugin implements IMixinConfigPlugin {
 
         return isClient && loader.isModLoaded("iris"); // With transitive Sodium dependency.
     }
-
-
-
-    @Override
-    public void onLoad(String mixinPackage) { }
-
-    @Override
-    public String getRefMapperConfig() { return null; }
-
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) { }
-
-    @Override
-    public List<String> getMixins() { return null; }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) { }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) { }
 }
--- src/main/java/retr0/bedrockwaters/compat/sodium/SodiumCompatMixinPlugin.java
+++ src/main/java/retr0/bedrockwaters/compat/sodium/SodiumCompatMixinPlugin.java
@@ -2,14 +2,9 @@ package retr0.bedrockwaters.compat.sodium;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import retr0.bedrockwaters.compat.AbstractMixinPlugin;
 
-import java.util.List;
-import java.util.Set;
-
-public class SodiumCompatMixinPlugin implements IMixinConfigPlugin {
+public class SodiumCompatMixinPlugin extends AbstractMixinPlugin {
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
         var loader = FabricLoader.getInstance();
@@ -19,24 +14,4 @@ public class SodiumCompatMixinPlugin implements IMixinConfigPlugin {
         // conditions for when we may not want to patch water opacity.
         return isClient && loader.isModLoaded("sodium") && !loader.isModLoaded("iris");
     }
-
-
-
-    @Override
-    public void onLoad(String mixinPackage) { }
-
-    @Override
-    public String getRefMapperConfig() { return null; }
-
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) { }
-
-    @Override
-    public List<String> getMixins() { return null; }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) { }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) { }
 }
